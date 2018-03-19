710b992b017051109791753cd1b6128cb2615167
Will Fang <will@hiwillfang.com>
2018-03-19 19:44:37 -0400

    Style skills component list and add global app vue styles

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
-    Skills
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
+      </ul>
+
+      <p>Here's a list of my technical skills!</p>
+    </div>
   </div>
 </template>
 
@@ -9,7 +15,13 @@ export default {
   name: 'skills',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      skills: [
+        { 'skill': 'Vue.js' },
+        { 'skill': 'PHP' }
+      ],
+      bgColor: 'blue',
+      bgWidth: '100%',
+      bgHeight: '50px'
     }
   }
 }
@@ -17,18 +29,28 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .holder {
+    background: #fff;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
 </style>
